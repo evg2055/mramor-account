312b4d4a62e5faaf9c15bc9c639cbe5789fd0996
EVG2055 <enivanoff55@mail.ru>
2022-08-18 00:30:33 +0600

    done function add options

--- form/test.js
+++ form/test.js
@@ -1,16 +1,25 @@
 let datalistListSetMaterial = document.getElementById("listSetMaterial");
 let optionMaterialsList = document.querySelectorAll('#listSetMaterial option');
 let inputMaterials = document.getElementById("setMaterial");
+let ButtonAddMaterialsList = document.querySelector('#ButtonAddMaterialsList');
+// let fieldsetNewOption = document.querySelector('#fieldsetNewOption');
 
-// let datalistListJobs = document.getElementById("listSetJobs");
-// let optionJobsList = document.querySelectorAll('#listSetJobs option');
-// let inputSetJobs = document.getElementById("setJobs");
+let modalContainer = document.querySelector('#modalContainer');
+let ButtonModalAdd = document.querySelector('#modalAdd');
+let ButtonModalClose = document.querySelector('#modalClose');
 
-// let done = document.getElementById('doneButton');
+let datalistListJobs = document.getElementById('listSetJobs');
+let optionJobsList = document.querySelectorAll('#listSetJobs option');
+let inputSetJobs = document.getElementById("setJobs");
+let ButtonAddJobsList = document.querySelector('#ButtonAddJobsList');
+
+
+let doneButton = document.querySelector('#doneButton');
 
 let materialsObj = {};
-// let jobsObj = {};
+let jobsObj = {};
 
+buttonOnclick(jobsObj, datalistListJobs, ButtonAddJobsList)
 class Option {
     name;
     price;
@@ -38,18 +47,53 @@ granite.addInObj(materialsObj);
 
 
 function renderOption(obj, parentOption ){
+    parentOption.innerHTML = '';
     for(let key in obj){
     let newOption = document.createElement('option');
     newOption.value = obj[key].name;
     parentOption.appendChild(newOption);
     }}
 
-    renderOption(materialsObj, datalistListSetMaterial);
+    
 // done.addEventListener('click', () => {
 //     console.log(materialsObj[inputMaterials.value].price)
 // }) ;
 
-function addOption () {
-    let newOption = new Option(inputName, inputPrice, inputPcs);
+function addOption (obj, parentOption) {
+    ButtonModalAdd.onclick = () => {
+    modalContainer.classList.toggle('modal__Container_isOpen');
+
+    let inputNameValue = document.querySelector('#inputName').value;
+    let inputPriceValue = document.querySelector('#inputPrice').value;
+    let inputPcsValue = document.querySelector('#inputPcs').value;
+
+    let newOption = new Option(inputNameValue, inputPriceValue, inputPcsValue);
+    newOption.addInObj(obj);
+
+    renderOption(obj, parentOption);
+    
+    // fieldsetNewOption.classList.toggle('fieldsetNewOption');   
+    }
 }
 
+function buttonOnclick (obj, parentOption, button) {
+        button.onclick = (event) => {
+        modalContainer.classList.add('modal__Container_isOpen');
+        event.preventDefault();
+        addOption(obj,parentOption);
+        ButtonModalClose.onclick = () => {
+            modalContainer.classList.remove('modal__Container_isOpen');
+            console.log(parentOption)
+        }
+    }
+};
+
+buttonOnclick(materialsObj, datalistListSetMaterial, ButtonAddMaterialsList);
+// ButtonAddMaterialsList.addEventListener ('click',(event) => {
+//     modalContainer.classList.add('modal__Container_isOpen');
+//     event.preventDefault();
+//     addOption(materialsObj, datalistListSetMaterial);
+//     ButtonModalClose.onclick = () => {
+//         modalContainer.classList.remove('modal__Container_isOpen');
+//     }
+// })
